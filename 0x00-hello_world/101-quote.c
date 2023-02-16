dfd8d5996e1e471f8cb6d177b61da81ae0e0a6aa
BlueMagicRoad <nathimaaga@gmail.com>
2023-02-16 02:01:31 -0800

    Optional task 2 correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include <unistd.h>
-#include <string.h>
 /**
  * main - Starting point of the program, prints text to standard error
  *
@@ -8,8 +7,6 @@
  */
 int main(void)
 {
-	int howLong = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
-
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", howLong);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
