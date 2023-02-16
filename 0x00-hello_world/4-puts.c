9ca403d9903880314155686a75c411c240c76f7e
BlueMagicRoad <nathimaaga@gmail.com>
2023-02-16 00:32:54 -0800

    Task 4 correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
-
-int main(void){
+/**
+ * The starting point of the program is main
+ *
+ * It will return 0 if run successfully , and a non-zero value if not
+ */
+int main(void)
+{
 	char task1[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(task1);
 	return (0);
 }
