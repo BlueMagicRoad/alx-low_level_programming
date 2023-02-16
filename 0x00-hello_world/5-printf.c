99f243681d011825a534e4473d13f0126d872bab
BlueMagicRoad <nathimaaga@gmail.com>
2023-02-16 00:49:45 -0800

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - Starting point of the program, prints the text held in task1
+ *
+ * Return: 0 if successful, non-zero if error(s)
+ */
+int main(void)
+{
+        char task1[] = "with proper grammar, but the outcome is a piece of art,";
+
+        printf(task1);
+        return (0);
+}
