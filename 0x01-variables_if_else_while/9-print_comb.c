b704cbe5af96ee65f8398862529b300425d9f130
BlueMagicRoad <nathimaaga@gmail.com>
2023-02-17 12:27:49 -0800

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+/**
+ * main - Starting point of the program
+ *
+ * Description: Print all single digits in base 10, separated by ', '
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+	
+		putchar(n);
+		if (n < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
