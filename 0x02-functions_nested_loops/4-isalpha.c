12037317143fb043a389987c1c764721413b0eba
BlueMagicRoad <nathimaaga@gmail.com>
2023-02-21 01:42:34 -0800

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isalpha - Check if character c is a letter
+ * @c: char type
+ *
+ * Return: 1 if letter, 0 otherwise
+ * */
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
