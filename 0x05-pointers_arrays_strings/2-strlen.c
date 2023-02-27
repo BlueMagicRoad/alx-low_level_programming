cba1bd49aac3f21573ca95d38a4321f4649f5497
BlueMagicRoad <nathimaaga@gmail.com>
2023-02-27 01:07:05 -0800

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * int_strlen - Prints the length of string s
+ * @s: char type pointer
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int x;
+
+	for (x = 0; s[x] != '\0'; x++)
+		;
+
+	return (x);
+}
